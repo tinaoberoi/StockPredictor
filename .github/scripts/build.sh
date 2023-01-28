1d237f68fc2bf84caf5f2fa6e8a2a17761913316
Tina <tina@Tinas-MacBook-Air.local>
2023-01-28 17:55:50 -0600

    Add github workflows

---
+++ .github/scripts/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Creating a virtual env for you."
+python3 -m venv env
+
+echo -ne '#####                     (33%)\r'
+sleep 1
+
+echo "Activating virtual environment env."
+#source env/bin/activate
+echo -ne '#############             (66%)\r'
+sleep 1
+
+echo "Installed required packages."
+#pip3 install -r requirements.txt
+echo "Building your model."
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+
+python3 create_model.py
+echo "Starting the app."
+python3 app.py
+
